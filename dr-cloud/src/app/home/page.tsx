06818fd3692fcfc4070b6c77cad912b104ff14e0
Daniel S Jeremiah <danielshunom2@gmail.com>
2024-10-15 21:03:43 -0400

    initialized backend files

--- dr-cloud/src/app/home/page.tsx
+++ dr-cloud/src/app/home/page.tsx
@@ -7,10 +7,8 @@ export default function Home () {
     return (
         <div >
             <div className="fixed bottom-4 w-[90%] max-w-2xl left-1/2 transform -translate-x-1/2 p-4">
-  <div className="backdrop-blur-lg bg-white/30 border border-white/20 rounded-2xl shadow-lg p-4 max-h-32 overflow-y-auto">
-    <ChatBox />
-  </div>
-</div>
+                <ChatBox />
+            </div>
 
             <div className="flex flex-row space-x-4 justify center scroll-auto">
                 < HomeBento />
