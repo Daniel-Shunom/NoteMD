ad782f90339c44ae3b64c379fb991e5942dd3992
Daniel S Jeremiah <danielshunom2@gmail.com>
2024-10-22 23:13:21 -0400

    intialized mongodb client

--- landing_page/lib/mongodb.ts
+++ landing_page/lib/mongodb.ts
@@ -7,25 +7,22 @@ if (!process.env.MONGODB_URI) {
 const uri = process.env.MONGODB_URI;
 const options = { appName: "devrel.template.nextjs" };
 
-let client: MongoClient;
+let client: MongoClient | null = null;
 
-if (process.env.NODE_ENV === "development") {
-  // In development mode, use a global variable so that the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  let globalWithMongo = global as typeof globalThis & {
-    _mongoClient?: MongoClient;
-  };
+export const connectMongoDB = async () => {
+  try {
+    if (!client) {
+      // Initialize MongoClient only if it's not already initialized
+      client = new MongoClient(uri, options);
+    }
 
-  if (!globalWithMongo._mongoClient) {
-    globalWithMongo._mongoClient = new MongoClient(uri, options);
+    // Connect to the MongoDB server
+    await client.connect();
+    console.log('Connected to MongoDB');
+    
+    return client; // Return the client instance if needed
+  } catch (error) {
+    console.error('Failed to connect to MongoDB:', error);
+    throw new Error('Failed to connect to MongoDB');
   }
-  client = globalWithMongo._mongoClient;
-} else {
-  // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri, options);
-}
-
-// Export a module-scoped MongoClient. By doing this in a
-// separate module, the client can be shared across functions.
-
-export default client;
+};
