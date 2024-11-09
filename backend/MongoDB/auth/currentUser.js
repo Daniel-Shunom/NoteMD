ff9143af18dff06dcf9bdea24fb0605c816be6db
Daniel S Jeremiah <danielshunom2@gmail.com>
2024-11-09 08:15:52 -0500

    removed password logging

--- backend/MongoDB/auth/currentUser.js
+++ backend/MongoDB/auth/currentUser.js
@@ -1,3 +1,5 @@
+// controllers/currentUser.js
+
 import express from 'express';
 import { authenticateToken } from '../middleware/auth.js';
 import User from '../models/user_model.js';
@@ -7,14 +9,12 @@ const router = express.Router();
 // GET /api/currentUser
 router.get('/api/currentUser', authenticateToken, async (req, res) => {
   try {
-    const user = await User.findById(req.user.userId)
-      .select('-password') // Exclude password
-      .populate('patients', 'name lname email') // Populate patients if the user is a doctor
-      .populate('doctor', 'name lname email licenseNumber'); // Populate doctor if the user is a patient
+    const user = await User.findById(req.user.userId).select('-password'); // Exclude password
 
     if (!user) {
       return res.status(404).json({ status: 'error', message: 'User not found.' });
     }
+
     res.status(200).json({ status: 'ok', user });
   } catch (error) {
     console.error('Error fetching current user:', error);
